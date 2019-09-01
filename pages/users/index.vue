adeb000a3433b9e13cc898a6fd767abdc6bdd3d9
tatsuya <kmnswimert@gmail.com>
2019-09-01 14:54:47 +0900

    [fix] add postpage

--- pages/users/index.vue
+++ pages/users/index.vue
@@ -2,6 +2,7 @@
     <div>
         <p>
             /users/index.vue
+            <!-- ブログ記事が並ぶとこ -->
         </p>
     </div>
 </template>
---
+++ pages/users/post.vue
@@ -0,0 +1,5 @@
+<template>
+    <div>
+        <!-- 新しい記事の投稿をするところ -->
+    </div>
+</template>
